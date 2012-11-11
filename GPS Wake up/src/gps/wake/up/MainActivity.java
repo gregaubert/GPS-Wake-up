04d5498d38c0e0caa20c0c6844058e7a865f92ba
Grégoire Aubert <gregoire.aubert@heig-vd.ch>
2012-11-11 21:35:29 +0100

    Map avec point qui s'affiche mais les cercles ne fonctionnent pas encore.

--- GPS Wake up/src/gps/wake/up/MainActivity.java
+++ GPS Wake up/src/gps/wake/up/MainActivity.java
@@ -1,14 +1,22 @@
 package gps.wake.up;
 
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends MapActivity {
 
 	private MapView mapView;
+	private MyLocationOverlay myLocation;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,32 @@ public class MainActivity extends MapActivity {
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.icon_blue);
+        MyItemizedOverlay itemizedoverlay = new MyItemizedOverlay(drawable, this);
+        
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+        
+        GeoPoint point2 = new GeoPoint(35410000, 139460000);
+        OverlayItem overlayitem2 = new OverlayItem(point2, "Sekai, konichiwa!", "I'm in Japan!");
+        
+        myLocation = new MyLocationOverlay(getApplicationContext(), mapView);
+        myLocation.enableMyLocation();
+        
+        myLocation.runOnFirstFix(new Runnable() { 
+        	public void run() {
+        		mapView.getController().animateTo(myLocation.getMyLocation());
+        	}
+        });
+                
+        itemizedoverlay.addOverlay(overlayitem);
+        itemizedoverlay.addOverlay(overlayitem2);        
+        mapOverlays.add(itemizedoverlay);
+        mapOverlays.add(myLocation);
+        mapView.setLongClickable(true);
+        
     }
 
     @Override
