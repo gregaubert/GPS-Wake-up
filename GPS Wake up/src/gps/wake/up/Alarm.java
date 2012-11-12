572d1d8d143bea42bcb7cd2a1728bc1d540d75d9
Grégoire Aubert <gregoire.aubert@heig-vd.ch>
2012-11-12 21:30:26 +0100

    Création de la BD SQLite

---
+++ GPS Wake up/src/gps/wake/up/Alarm.java
@@ -0,0 +1,88 @@
+package gps.wake.up;
+
+import com.google.android.maps.GeoPoint;
+
+public class Alarm {
+	
+	private int id;
+	private String name;
+	private GeoPoint location;
+	private boolean enabled;
+	private int radius;
+	private boolean vibrator;
+	private String alarmName;
+	private int volume;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public GeoPoint getLocation() {
+		return location;
+	}
+
+	public void setLocation(GeoPoint location) {
+		this.location = location;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public boolean isVibrator() {
+		return vibrator;
+	}
+
+	public void setVibrator(boolean vibrator) {
+		this.vibrator = vibrator;
+	}
+
+	public String getAlarmName() {
+		return alarmName;
+	}
+
+	public void setAlarmName(String alarmName) {
+		this.alarmName = alarmName;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public Alarm() {}
+	
+	public Alarm(int id, GeoPoint location, int radius){
+		this.id = id;
+		this.location = location;
+		this.radius = radius;
+	}
+
+}
---
+++ GPS Wake up/src/gps/wake/up/BDSQLite.java
@@ -0,0 +1,39 @@
+package gps.wake.up;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BDSQLite extends SQLiteOpenHelper {
+	
+	// Global settings
+	private static final int DB_VERSION = 2;
+	private static final String DB_NAME = "BD_GPSWakeUp";
+	
+	// Alarms table creation
+	private static final String CREATE_TABLE_ALARMS = 
+			"CREATE TABLE " + AlarmBD.TABLE_ALARMS + " (" +
+					AlarmBD.COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+					AlarmBD.COL_NAME + " TEXT," + AlarmBD.COL_LAT + " INTEGER NOT NULL, " +
+					AlarmBD.COL_LONG + " INTEGER NOT NULL, " + AlarmBD.COL_ENABLED + " INTEGER, " + 
+					AlarmBD.COL_RADIUS + " INTEGER NOT NULL, " + AlarmBD.COL_VIBRATOR + "INTEGER, " + 
+					AlarmBD.COL_ALARM_NAME + " TEXT, " + AlarmBD.COL_VOLUME + " INTEGER);";
+	
+	public BDSQLite(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_TABLE_ALARMS);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		//On peut fait ce qu'on veut ici moi j'ai décidé de supprimer la table et de la recréer
+		//comme ça lorsque je change la version les id repartent de 0
+		db.execSQL("DROP TABLE " + AlarmBD.TABLE_ALARMS + ";");
+		onCreate(db);
+	}
+
+}
